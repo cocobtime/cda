5ebebabb236a62393772df4c7c13fd1eb26d61e3
cocobtime <cocopolice@gmail.com>
2023-11-11 19:53:33 +0000

    Your commit message here

--- js/header.js
+++ js/header.js
@@ -1,94 +1,20 @@
+// header-component.js
+
 const headerTemplate = document.createElement('template');
 
 headerTemplate.innerHTML = `
-  <style>
-
-
-  
-    nav {
-      height: 40px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color:#D115F4;
-      width:100%;
- 
-
-
-    }
-
-    h1, h2{
-      color: #D115F4;
-      }
-
-    ul {
-      padding: 0;
-    }
-    
-    ul li {
-      list-style: none;
-      display: inline;
-    }
-    
-    a {
-      font-weight: 700;
-      margin: 0 15px;
-      color: #fff;
-      text-decoration: none;
-    }
-    
-    a:hover {
-      padding-bottom: 5px;
-      box-shadow: inset 0 -2px 0 0 #fff;
-    }
-
-    img {
-      width: 100px;
-      height: 100px;
-    }
-
-    header {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      background-image: url('/assets/bg.webp');
-      background-size: cover;
-      background-repeat: no-repeat;
-      background-position: center;
-    }
-
-
-    
-
-    .logo-container {
-      display: flex;
-      align-items: center;
-    }
-
-    .logo-container h1 {
-      margin-left: 10px;  // space between the logo and the title
-    }
-    
-  </style>
   <header>
     <div class="logo-container">
       <img src="/assets/logo.png" alt="Coeur D'amour logo">
       <h1>Coeur D'amour</h1>
-
     </div>
     <div id="mySidepanel" class="sidepanel">
-
-
-    <a href="index.html">Home</a>
-    <a href="about.html">About</a>
-    <a href="mission.html">Mission</a>
-    <a href="contact.html">Contact</a>
-    <a href="donation.html">Donation</a>
-    
-
+      <a href="index.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="mission.html">Mission</a>
+      <a href="contact.html">Contact</a>
+      <a href="donation.html">Donation</a>
     </div>
-
-
   </header>
 `;
 
@@ -100,7 +26,7 @@ class Header extends HTMLElement {
   connectedCallback() {
     const shadowRoot = this.attachShadow({ mode: 'closed' });
 
-    shadowRoot.appendChild(headerTemplate.content);
+    shadowRoot.appendChild(headerTemplate.content.cloneNode(true));
   }
 }
 
