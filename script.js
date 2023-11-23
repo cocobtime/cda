0e2368941bc570530f791c0e93dedd4e300db5a3
cocobtime <cocopolice@gmail.com>
2023-11-23 19:06:11 +0000

    Your commit message here

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.querySelector('.nav');
-
-    menuToggle.addEventListener('change', function() {
-        if (this.checked) {
-            nav.style.display = 'flex';
-        } else {
-            nav.style.display = 'none';
-        }
-    });
-});
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
