a88ff4aae0c827fe5df24ac5f9359099d7db758d
cocobtime <cocopolice@gmail.com>
2023-11-25 20:40:12 +0000

    Your commit message here

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function toggleMenu() {
-    var navbar = document.getElementById("navbar");
-    if (navbar.style.display === "block") {
-      navbar.style.display = "none";
+function toggleMobileMenu() {
+    const navbar = document.getElementById('mobile-menu');
+    if (navbar.style.display === 'block') {
+        navbar.style.display = 'none';
     } else {
-      navbar.style.display = "block";
+        navbar.style.display = 'block';
     }
-  }
-  
+}
